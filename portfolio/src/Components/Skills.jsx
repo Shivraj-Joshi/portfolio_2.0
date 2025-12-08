84f7685f9bf17a6d2d66e480050434177b3d36ed
Shivraj joshi <shibujoshi07@gmail.com>
2025-12-08 23:48:17 +0530

    skills component restructured

--- portfolio/src/Components/Skills.jsx
+++ portfolio/src/Components/Skills.jsx
@@ -1,38 +1,73 @@
 import React from "react";
-
+import { skills } from "../temp/Data";
 const Skills = () => {
   return (
     <>
-      <div className="bg-[#141414] h-[150px] md:h-[500px] md:w-[600px] p-3 rounded-lg">
+      <div className="bg-[#141414] h-auto md:h-auto md:w-[600px] p-3 rounded-lg">
         <div className="p-2 border-b-2 border-[#5237f9]">
           <h1 className="text-center font-bold text-white font-myfont text-[30px]">
             Skills
           </h1>
         </div>
-        <div className=" grid grid-flow-col grid-rows-3 gap-4 p-4 bg-amber-100">
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2 ">
-            react
+        <div className="  mt-1 p-2 flex flex-col gap-2">
+          {/* frontend skills div  */}
+          <div className="frontend-div  p-6 rounded">
+            <h2 className="font-myfont text-center text-white mb-4">
+              Frontend Skills
+            </h2>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+              {skills
+                .find((item) => item.category === "Frontend")
+                ?.items?.map((skill) => (
+                  <div
+                    key={skill}
+                    className="bg-green-700 text-white text-center py-2 px-3 rounded"
+                  >
+                    {skill}
+                  </div>
+                ))}
+            </div>
           </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
+
+          {/* backend skills div */}
+          <div className="backend-div  p-6 rounded">
+            <h2 className="font-myfont text-center text-white mb-4">
+              Backend Skills
+            </h2>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+              {skills
+                .find((item) => item.category === "Backend")
+                ?.items?.map((skill) => (
+                  <div
+                    key={skill}
+                    className="bg-green-700 text-white text-center py-2 px-3 rounded"
+                  >
+                    {skill}
+                  </div>
+                ))}
+            </div>
           </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
-          </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
-          </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
-          </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
-          </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
-          </div>
-          <div className="h-20 w-20 rounded-full bg-green-700 text-white  text-center p-2">
-            react
+
+          {/* dev-tools div */}
+          <div className="frontend-div  p-6 rounded">
+            <h2 className="font-myfont text-center text-white mb-4">
+              Developer Tools
+            </h2>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+              {skills
+                .find((item) => item.category === "devTools")
+                ?.items?.map((skill) => (
+                  <div
+                    key={skill}
+                    className="bg-green-700 text-white text-center py-2 px-3 rounded"
+                  >
+                    {skill}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
---
+++ portfolio/src/temp/Data.js
@@ -0,0 +1,47 @@
+const skills = [
+
+    {
+        items: ["HTML", "CSS", "JavaScript", "React", "Tailwind", "Redux", "Framer Motion"],
+        category: "Frontend"
+    },
+    {
+        items: ["NodeJs", "Express", "Mongoose", "MongoDB"],
+        category: "Backend"
+    },
+    {
+        items: ["VsCode", "Git", "Github"],
+        category: "devTools"
+    }
+
+]
+
+const project = [
+
+    {
+
+        name: "AI chatBot",
+        tech: ["React", "Tailwind Css", "Google Gemini API"],
+        live: "URL",
+        gitHub: "URL"
+
+    },
+    {
+
+        name: "StyleClub",
+        tech: ["React", "Tailwind Css", "Firebase", "Redux"],
+        live: "URL",
+        gitHub: "URL"
+
+    },
+    {
+
+        name: "Portfolio",
+        tech: ["React", "Tailwind Css"],
+        live: "URL",
+        gitHub: "URL"
+
+    },
+
+]
+
+export { skills };
