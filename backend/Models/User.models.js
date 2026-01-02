0916c64bd49ba1f07755a2cf049795529066ac1c
Shivraj joshi <shibujoshi07@gmail.com>
2026-01-02 16:21:20 +0530

    used bcrypt to hash password

--- backend/Models/User.models.js
+++ backend/Models/User.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
 
@@ -28,6 +30,55 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true })
 
-const User = mongoose.model('User', userSchema);
+// middleware used to encrypt the password just before saving the user details 🔐
 
-export default User;
+userSchema.pre("save", async function (next) {
+
+    if (this.modified("password")) {
+        this.password = bcrypt.hash(this.password, 10)
+        next()
+    }
+
+})
+
+// methode to check whether the password is correct or not 
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    bcrypt.compare(password, this.password)
+}
+
+//method for generating  access token 
+
+userSchema.method.generateAccessToken = function () {
+    jwt.sign({
+        _id: this._id,
+        name: this.name,
+        email: this.email
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+//method for generating Refresh Token
+
+userSchema.method.generateRefreshToken = function () {
+    jwt.sign({
+        _id: this._id,
+
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+
+        {
+            expiresIn: process.env.REFERSH_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+export const User = mongoose.model('User', userSchema);
+
